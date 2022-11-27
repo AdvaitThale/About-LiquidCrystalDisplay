ad4848d76df061c815fbc1daec041e24de5542b2
Advait Thale <advaitthale98@gmail.com>
2022-11-28 00:05:43 +0530

    updated

--- LCD_From_Scratch/LCD_From_Scratch.ino
+++ LCD_From_Scratch/LCD_From_Scratch.ino
@@ -12,6 +12,10 @@
   ------------------------------------------------------------------------------
   |5V|GND|Contrast|Register Select|R/W|Enable|D0|D1|D2|D3|D4|D5|D6|D7|LED+|LED-| 
   ------------------------------------------------------------------------------
+  
+  There are 2 modes:
+  Instruction Mode -> To configure LCD settings, cursor position, etc.
+  Data Mode -> To write data on LCD
 */
 
 
